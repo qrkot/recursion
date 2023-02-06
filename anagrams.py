8f5c7edfd89d67ada4e1da9e467d5c29b3b458ca
me <me@alma8.vm>
2023-02-06 10:44:59 +0300

    Anagram commit

---
+++ anagrams.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+# Print anagrams of a given string.
+#
+
+import sys
+
+def anagrams_of(string):
+
+    if len(string) == 1:
+        return [string[0]]
+
+    collection = []
+
+    substring_anagrams = anagrams_of(string[1 : len(string)])
+
+    for substring_anagram in substring_anagrams:
+        for index in range(len(substring_anagram) + 1):
+            collection.append(substring_anagram[:index]+string[0]+substring_anagram[index:])
+
+    return collection
+        
+
+print( anagrams_of( sys.argv[1] if len(sys.argv) > 1 else '123' ) )
